bea0a93b30cc567669960478b89dcaa3b41a436b
elenamlahoz <violinistarara@hotmail.com>
2015-07-19 13:10:39 +0200

    Versión 3
    -Paso 1: Añadir MVC de pregunta y respuesta
    a: Crear directorio controllers y el controlador quiz_controller.js
    b: Importar quiz_controller.js en routes/index.js.js
    c: Introducir nuevas rutas en el enrutador routes/index.js
    Rutas: GET /quizes/question y GET /quizes/answer
    d: Crear directorio view/quizes y las vistas question.ejs y answer.ejs
    -Paso 2: Añadimos enlace a la pregunta en la página de entrada
    Así enlazamos la pregunta desde la página de entrada (hipermedia)
    -Paso 3: Guardamos versión (commit) con git

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizControler=require('../controllers/quizControler');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizControler.question);
+router.get('/quizes/answer', quizControler.answer);
 
 module.exports = router;
