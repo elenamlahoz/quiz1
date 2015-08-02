b2c2d3e0db27ca8cf038fc46f4980a3a35b09b92
elenamlahoz <violinistarara@hotmail.com>
2015-08-02 17:09:08 +0200

    Guardo tb author

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var quizControler=require('../controllers/quiz_controller');
 
+var autControler=require('../controllers/aut_controller');
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -10,5 +13,8 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizControler.question);
 router.get('/quizes/answer', quizControler.answer);
+
+
+router.get('/author', autControler.author);
 
 module.exports = router;
