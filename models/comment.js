8ae29d2c14aa1120cf8d6dc8a2275268d6d91391
elenamlahoz <violinistarara@hotmail.com>
2015-08-08 11:21:19 +0200

    Comentarios

---
+++ models/comment.js
@@ -0,0 +1,9 @@
+//Definición del modelo de quiz
+module.exports=function(sequelize, DataTypes){
+	return sequelize.define('Comment',{
+		texto: {
+			type: DataTypes.STRING,
+			validate: {notEmpty:{msg:"-->Falta comentario"}}
+		}
+	});
+}
